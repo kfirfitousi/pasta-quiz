4b790be3d1c3571db9cbb05de9e7a2d1ca7b8978
kfirfitousi <kfirp84@gmail.com>
2022-08-12 17:45:56 +0300

    Updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
+import Head from 'next/head';
 import Spinner from 'components/Spinner';
 
 import '../styles/globals.css';
@@ -32,11 +33,19 @@ const PastaQuiz = ({ Component, pageProps }: AppProps) => {
 
     return (
         <>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+            </Head>
+
             {loading ? (
                 <div className="w-screen h-20 absolute inset-y-10 sm:inset-0 -z-10 flex items-center text-yellow-800">
                     <Spinner size={80} />
                 </div>
             ) : null}
+
             <Component {...pageProps} />
         </>
     );
