3eda87b29f9a56ce9d87aa622fe8aeffd43e7e4d
kfirfitousi <kfirp84@gmail.com>
2022-08-18 00:22:02 +0300

    Fixed header on mobile

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -13,13 +13,13 @@ const Header = () => {
 
     return (
         <header className="py-6 text-yellow-800 select-none">
-            <nav className="flex items-center space-x-1">
+            <nav className="flex items-center space-x-0 md:space-x-1">
                 <h2 className="text-3xl font-title font-bold min-w-fit mr-auto">Pasta Quiz</h2>
                 {routes.map(([path, name], index) => (
                     <Link href={path} key={index}>
                         <a
                             className={`
-                            hover:text-yellow-300 hover:bg-yellow-800 p-2 rounded
+                            hover:text-yellow-300 hover:bg-yellow-800 p-1 md:p-2 rounded
                             ${router.asPath === path ? 'underline underline-offset-8' : ''}
                             `}
                         >
