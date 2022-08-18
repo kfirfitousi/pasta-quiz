84f07736c4351e498e00f6192d2a5fbb856cb62e
kfirfitousi <kfirp84@gmail.com>
2022-08-18 09:38:53 +0300

    Fixed footer images

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,37 +1,54 @@
 import Image from 'next/image';
+import type { ImageLoader } from 'next/image';
+
+const badgeLoader: ImageLoader = ({ src, width, quality }) => {
+    return `https://img.shields.io/badge/${src}&w=${width}&q=${quality || 75}`;
+};
 
 const Footer = () => {
     return (
         <footer>
             <div className="flex flex-col text-center text-yellow-800">
+                <span className="mb-2 md:mr-2 md:mb-0 w-full md:w-auto">Built with</span>
                 <div className="flex flex-row flex-wrap md:flex-nowrap justify-center items-center mx-auto my-2">
-                    <span className="mb-2 md:mr-2 md:mb-0 w-full md:w-auto">Built with</span>
                     <Image
-                        src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+                        src="react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+                        loader={badgeLoader}
                         alt="React"
                         width="86.25"
                         height="28"
                     />
                     <Image
-                        src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+                        src="Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+                        loader={badgeLoader}
                         alt="Next.js"
                         width="78"
                         height="28"
                     />
                     <Image
-                        src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+                        src="typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+                        loader={badgeLoader}
                         alt="TypeScript"
                         width="126.5"
                         height="28"
                     />
                     <Image
-                        src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+                        src="tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+                        loader={badgeLoader}
                         alt="TailwindCSS"
                         width="138.75"
                         height="28"
                     />
                     <Image
-                        src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white"
+                        src="Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white"
+                        loader={badgeLoader}
+                        alt="Supabase"
+                        width="113"
+                        height="28"
+                    />
+                    <Image
+                        src="vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white"
+                        loader={badgeLoader}
                         alt="Vercel"
                         width="93.5"
                         height="28"
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['img.shields.io']
-    }
+    reactStrictMode: true
 };
 
 module.exports = nextConfig;
