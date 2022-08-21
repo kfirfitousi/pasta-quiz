5a8e534e824d863dc8f4bd79484fe90347d7081f
kfirfitousi <kfirp84@gmail.com>
2022-08-21 18:22:16 +0300

    Fixed config

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://pasta.kfirfitousi.com';
+export const server = dev ? 'http://localhost:3000' : process.env.VERCEL_URL;
