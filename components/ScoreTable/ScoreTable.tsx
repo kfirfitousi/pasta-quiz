5e261c4c554b5cbd8bdff6cf9bf6c52b4c1427df
kfirfitousi <kfirp84@gmail.com>
2022-08-23 18:12:10 +0300

    UI tweaks

--- components/ScoreTable/ScoreTable.tsx
+++ components/ScoreTable/ScoreTable.tsx
@@ -5,7 +5,7 @@ import Spinner from '~/Spinner';
 
 const ScoreTable = () => {
     const [limit, setLimit] = useState(10);
-    const { data, isError } = useLeaderboard({
+    const { data, isFetching, isError, isPreviousData } = useLeaderboard({
         limit,
         config: { keepPreviousData: true }
     });
@@ -13,17 +13,18 @@ const ScoreTable = () => {
     if (isError) {
         return (
             <div className="text-center text-yellow-800 mt-5">
-                Error occured while fetching leaderboard.
+                <p>Error occured while fetching leaderboard.</p>
+                <p>Try refreshing the page.</p>
             </div>
         );
     }
 
     return (
         <section className="flex flex-col items-center mt-5 text-yellow-800">
-            <div className="flex flex-row text-center md:h-8">
+            <div className="flex flex-row text-center h-6 md:h-8">
                 <p className="w-6 md:w-10"></p>
-                <p className="w-52 md:w-80 md:leading-8 md:text-lg">Name</p>
-                <p className="w-20 md:leading-8 md:text-lg">Score</p>
+                <p className="w-52 md:w-80 md:text-lg">Name</p>
+                <p className="w-20 md:text-lg">Score</p>
             </div>
 
             <div className="flex flex-col space-y-1.5">
@@ -45,14 +46,13 @@ const ScoreTable = () => {
                 ))}
             </div>
 
-            {limit <= 40 && (
-                <button
-                    className="h-9 px-3 mt-3 shadow-md rounded bg-yellow-300 text-yellow-800 hover:bg-yellow-800 hover:text-yellow-300"
-                    onClick={() => setLimit(limit + 10)}
-                >
-                    Show More
-                </button>
-            )}
+            <button
+                className="h-9 px-3 mt-3 shadow-md rounded bg-yellow-300 text-yellow-800 hover:bg-yellow-800 hover:text-yellow-300"
+                disabled={isPreviousData}
+                onClick={() => setLimit((prev) => prev + 10)}
+            >
+                {isFetching ? <Spinner size="sm" /> : 'Show More'}
+            </button>
         </section>
     );
 };
