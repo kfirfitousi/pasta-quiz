3b6a9d591014bac8befb20b85202b1deb4573814
kfirfitousi <kfirp84@gmail.com>
2022-08-28 16:53:45 +0300

    Fix axios config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,9 @@
 import Axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+const API_URL =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:3000'
+        : (process.env.API_URL as string);
 
 export const axios = Axios.create({
     baseURL: API_URL
