460fed131506ec97fda4686b5b79d5b27cd51023
kfirfitousi <kfirp84@gmail.com>
2022-08-28 16:35:12 +0300

    Provide fallback for API_URL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 
-const API_URL = process.env.API_URL as string;
+const API_URL = process.env.API_URL || 'http://localhost:3000';
 
 export const axios = Axios.create({
     baseURL: API_URL
